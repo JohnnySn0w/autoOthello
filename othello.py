3c4fd876e98755fb494ecdf72c5404e6f0247719
JohnnySn0w <michael.mahan43@gmail.com>
2020-11-08 16:18:41 -0600

    init

---
+++ othello.py
@@ -0,0 +1,9 @@
+
+''' autoOthello, an implementation of Othello that also has an AI player who uses MiniMax, optionally with A/B pruning
+ * Name: Michael Mahan
+ * Student ID: 102-36-293
+ * November 10, 2020
+ * Assignment 3
+ * Class: CSC-475
+ * Professor: Dr. Mike O'Neal
+'''
